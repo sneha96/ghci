136086dce0d03294027312ef19687522c93c56d5
arushigarg <arushigarg62@gmail.com>
2015-10-05 22:51:47 +0530

    edited forms

--- neev.sql
+++ neev.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2015 at 04:25 PM
+-- Generation Time: Sep 28, 2015 at 06:37 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -131,7 +131,15 @@ CREATE TABLE IF NOT EXISTS `raw_mat` (
   `tot_qty` int(11) NOT NULL,
   `tot_cost` int(11) NOT NULL,
   `no_of_var` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `raw_mat`
+--
+
+INSERT INTO `raw_mat` (`raw_id`, `name`, `unit`, `desc`, `tot_qty`, `tot_cost`, `no_of_var`) VALUES
+(1, 'abcd', 'kg', 'bekar samaan', 5, 500, 1),
+(2, 'qwer', 'kg', 'sahi samaan', 5, 500, 0);
 
 -- --------------------------------------------------------
 
@@ -146,7 +154,15 @@ CREATE TABLE IF NOT EXISTS `raw_mat_trans` (
   `expected_date` datetime NOT NULL,
   `received_date` datetime NOT NULL,
   `tot_cost` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `raw_mat_trans`
+--
+
+INSERT INTO `raw_mat_trans` (`trans_id`, `supp_id`, `order_date`, `expected_date`, `received_date`, `tot_cost`) VALUES
+(1, 1, '2015-09-28 00:00:00', '2015-09-30 00:00:00', '0000-00-00 00:00:00', 500),
+(2, 1, '2015-09-30 00:00:00', '2015-10-03 00:00:00', '0000-00-00 00:00:00', 500);
 
 -- --------------------------------------------------------
 
@@ -161,8 +177,19 @@ CREATE TABLE IF NOT EXISTS `raw_mat_trans_var` (
   `var_id` int(11) NOT NULL,
   `order_qty` int(11) NOT NULL,
   `received_qty` int(11) NOT NULL,
-  `cost_per_unit` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `cost_per_unit` int(11) NOT NULL,
+  `raw_name` varchar(100) NOT NULL,
+  `var_name` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `raw_mat_trans_var`
+--
+
+INSERT INTO `raw_mat_trans_var` (`sno`, `trans_id`, `raw_id`, `var_id`, `order_qty`, `received_qty`, `cost_per_unit`, `raw_name`, `var_name`) VALUES
+(1, 1, 1, 1, 5, 0, 100, 'abcd', 'cheap'),
+(2, 2, 2, 0, 5, 0, 100, 'qwerty', ''),
+(3, 1, 3, 3, 3, 0, 60, 'xqwe', 'rewd');
 
 -- --------------------------------------------------------
 
@@ -177,7 +204,14 @@ CREATE TABLE IF NOT EXISTS `raw_variant` (
   `price` int(11) NOT NULL,
   `avail_qty` int(11) NOT NULL,
   `min_qty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `raw_variant`
+--
+
+INSERT INTO `raw_variant` (`var_id`, `raw_id`, `name`, `price`, `avail_qty`, `min_qty`) VALUES
+(1, 1, 'cheap', 50, 5, 10);
 
 -- --------------------------------------------------------
 
@@ -193,7 +227,14 @@ CREATE TABLE IF NOT EXISTS `supplier` (
   `city` varchar(100) NOT NULL,
   `state` varchar(100) NOT NULL,
   `remarks` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`supp_id`, `name`, `contact_no`, `address`, `city`, `state`, `remarks`) VALUES
+(1, 'sham', '9999999999', 'uk', 'uh', 'iiu', 'gud');
 
 -- --------------------------------------------------------
 
@@ -321,27 +362,27 @@ MODIFY `var_id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `raw_mat`
 --
 ALTER TABLE `raw_mat`
-MODIFY `raw_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `raw_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `raw_mat_trans`
 --
 ALTER TABLE `raw_mat_trans`
-MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `raw_mat_trans_var`
 --
 ALTER TABLE `raw_mat_trans_var`
-MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `raw_variant`
 --
 ALTER TABLE `raw_variant`
-MODIFY `var_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `var_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `supplier`
 --
 ALTER TABLE `supplier`
-MODIFY `supp_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `supp_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `supplier_raw_mat`
 --
